cf629a996dd17f033a47dcff986fc49d4e4ebbc4
Maxim <mxmglebov2005@gmail.com>
2023-06-18 15:18:08 +0300

    Created class Book and readXml function

--- SR2/main.cpp
+++ SR2/main.cpp
@@ -1,10 +1,53 @@
-/*
- * main.cpp
- *
- *  Created on: 18 июн. 2023 г.
- *      Author: mxmgl
- */
+#include <iostream>
+#include <string>
+#include <iomanip>
+#include <fstream>
+#include <vector>
+using namespace std;
+
+struct Publication
+{
+	int year;
+	string publisher;
+};
+
+class Book
+{
+private:
+	string author;
+	string title;
+	Publication publication;
+public:
+	Book(string fileName)
+	{
+
+		string line;
+		while (!fin.eof())
+		{
+			getline(fin, line);
+		}
+	}
+};
+
+vector<Book> readXml(const string &fileName)
+{
+	vector<Book> books;
+
+	string line;
+	ifstream fin;
+			fin.open(fileName);
+
+	while (!fin.eof())
+	{
+		getline(fin, line);
+	}
+
+	return books;
+}
+
+int main()
+{
 
 
-
-
+	return 0;
+}
